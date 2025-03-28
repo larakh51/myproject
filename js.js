1ed21bf5be35265f35ada00ad87487dd92e68664
larakh51 <larakh51@gmail.com>
2025-03-28 19:49:35 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,20 @@
+// alert('lara')
+// alert('khoury')
+// Output in one line
+document.writeln("lara ");
+// writeln - will be printed with a line skip
+document.writeln("khoury ");
+// const myObj = { firstname: "lara", lastname: "khoury" };
+// console.log(myObj);
+// Output information to the console
+console.log("lara khoury");
+const myArr = ["Othman", "Bisan", "noor" ];
+console.log(myArr);
+alert("lara");
+let isBoss = confirm("Do you want to delete it the prodact??");
+
+alert( isBoss ); // true if OK is pressed
+let admin ;
+let name = 'john';
+admin= name
+console.log(admin)
